838c3c338508de4c1cf051d4f094fa7f7aee69aa
gbxnga <iamblizzyy@gmail.com>
2023-12-20 15:46:07 +0000

    added Ads banner

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -92,7 +92,10 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               </dd>
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+              <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">
+                <h1>Learn Hand-On AWS Cloud Engineering <a target="_blank" href="https://nimbus.school">HERE</a></h1>
+                {children}
+                </div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={discussUrl(path)} rel="nofollow">
                   Discuss on Twitter
